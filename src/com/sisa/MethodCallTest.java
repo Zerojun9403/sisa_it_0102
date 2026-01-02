e5554a83541f7af7f54c20b854d5cfe2011600b2
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-02 12:50:36 +0900

    2026-01-02 수업내용추가

--- src/com/sisa/MethodCallTest.java
+++ src/com/sisa/MethodCallTest.java
@@ -2,11 +2,10 @@ package com.sisa;
 
 public class MethodCallTest {
 
-//	C:\sisa\java\workspace\day0102\bin>java com.sisa.MethodCallTest
-//	오류: javafx.application.Application 클래스에서 기본 메소드가 com.sisa.MethodCallTest이(가) 아닙니다.
-//	다음 형식으로 기본 메소드를 정의하십시오.
-//	public static void main(String[] args)
 	public static void main(String[] args) {
+		 // static == 정적메모리할당
+		
+		
 		//프로그램의 앤트리 포인트 
 		// 미션 A 클래스의 hello() 호출하기 
 	    // 호출 ===> 메서드명();
@@ -15,7 +14,7 @@ public class MethodCallTest {
 		
 		test.hello();
 		
-		A a = new A();
+		A a = new A(); //new == 동적메모리할당
 		// A클래스 객체생성 
 		// (클래스가 포험하고 있는 필드와 메서드들을 메모리에 할당하겠다!)
 		//==> A클래스를 사용할 준비를 끌마쳤다. 
