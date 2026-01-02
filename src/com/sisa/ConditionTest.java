f1e4fe00df3c14f7671532d5c3988d546ca426b2
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-02 18:09:37 +0900

    Scaaner 추가

--- src/com/sisa/ConditionTest.java
+++ src/com/sisa/ConditionTest.java
@@ -1,5 +1,7 @@
 package com.sisa;
 
+import java.util.Scanner;
+
 public class ConditionTest {
 	/*
 	 * ConditionTest.java (소스코드, 텍스트파일)
@@ -12,31 +14,32 @@ public class ConditionTest {
 	 */
 	public static void main(String[] args) {
 		boolean condition = true;
-		
-		if(condition) 
-		System.out.println("열심히 공부합시다.");
+
+		if (condition)
+			System.out.println("열심히 공부합시다.");
 		// condition 즉, 조건식의 결과가 참일때 실행할 문장이 단 1개이면 영역 괄호{ }를 생락
-		// 2개 이상이면 반드시 영역괄호{ } 명시해야함. { }  안쓰면 컴파일러가 알아서 담아준다. 
-		
+		// 2개 이상이면 반드시 영역괄호{ } 명시해야함. { } 안쓰면 컴파일러가 알아서 담아준다.
+
 		System.out.println("오늘도 수고했어!!!!");
 		System.out.println("=============================");
-		
+
 		boolean happy = false;
-		
-		if(happy) {
+
+		if (happy) {
 			System.out.println("나는행복합니다."); // happy=t 일때 실행
-		}else {
+		} else {
 			System.out.println("나는불행합니다."); // happy=f 일때 실행
 		}
-		
-		// 윤년계산기 ver 1.0
-		int year = 2026;
+
+		// 숫자 입력 받는 윤년계산기
+		System.out.println("년도을입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
 		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
 			System.out.println(year + "년은 윤년입니다.");
 		} else {
 			System.out.println(year + "년은 평년입니다.");
 		}
-		
 
 	}
 
